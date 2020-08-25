0c1f6ee024a6032a28af5249fb9f5c68e6c74f90
William Pannell <lpann5@gmail.com>
2020-08-25 05:09:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,12 @@ class App < Sinatra::Base
   end
 
   get '/friends' do
-    # Write your code here!
+    @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
+   
+    erb :friends
+  end
 
+  get '/test' do
+    erb :test
   end
 end
